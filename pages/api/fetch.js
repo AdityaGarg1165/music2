4d7b5fef273561fe9441315e523df777f6304fbc
AdityaGarg1165 <90836787+AdityaGarg1165@users.noreply.github.com>
2023-05-10 16:54:33 +0530

    Update fetch.js

--- pages/api/fetch.js
+++ pages/api/fetch.js
@@ -3,7 +3,7 @@ const SpotifyWebApi = require("spotify-web-api-node")
 export default async function handler(req, res) {
     const body = JSON.parse(req.body)
     const SpotifyApi = new SpotifyWebApi({
-        redirectUri:"https://music2-two.vercel.app//dashboard",
+        redirectUri:"https://music2-two.vercel.app/dashboard",
         clientId:"2d619d57084f437aa49f91cf7197f671",
         clientSecret:"d3ba863d303c401c84b3e8dca435704b"
       })
