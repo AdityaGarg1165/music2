5e0640450302e3a38c8d12209abe3585a03c18d7
AdityaGarg1165 <90836787+AdityaGarg1165@users.noreply.github.com>
2023-05-25 14:33:01 +0530

    🔥

--- pages/api/link.js
+++ pages/api/link.js
@@ -4,34 +4,27 @@ import * as fs from 'fs'
 const ytdl = require('ytdl-core')
 import { YouTube } from 'nodetube'
 const { youtube } = require("ytdownloader-fts");
+// const youtubedl = require('youtube-dl-exec')
+const youtubedl = require('@distube/youtube-dl')
 
-const youtubedl = require('youtube-dl-exec')
 export default async function handler(req, res) {
     const body = JSON.parse(req.body)
 
-youtubedl(body.val, {
+    youtubedl(body.val, {
   dumpSingleJson: true,
-  noCheckCertificates: true,
-  audioFormat:'mp3',
   noWarnings: true,
+  noCallHome: true,
+  noCheckCertificate: true,
   preferFreeFormats: true,
-  addHeader: [
-    'referer:youtube.com',
-    'user-agent:googlebot'
-  ]
-
-}).then(output => {
-    console.clear()
-    // console.log(output['requested_formats'][1])
-    res.status(200).json({url:output['requested_formats'][1]['url']})
-
-
+  youtubeSkipDashManifest: true,
+//   referer: 'https://example.com'
 })
+  .then(output => res.status(200).json({url:output['requested_formats'][1]['url']}))
 
 
     
-    // const resu = await youtube("https://www.youtube.com/watch?v=UFzpAh-x_IY&ab_channel=Discoverychannel")
-    // console.log(body.val);
+
+
 
     // })
 
