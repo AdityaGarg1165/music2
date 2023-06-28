c53baaf7784f5ee2607ed9596ba5b7e34398a469
mridulgargk <130525035+mridulgargkh@users.noreply.github.com>
2023-06-28 18:10:21 +0530

    first commit

--- pages/api/link.js
+++ pages/api/link.js
@@ -8,8 +8,9 @@ const ytdl = require('ytdl-core')
 export default async function handler(req, res) {
   const body = JSON.parse(req.body)
   const info = await ytdl.getInfo(body.val)
-    const vid = await ytdl.chooseFormat(info.formats,{quality:"highest"})
-    console.log(vid.mimeType)
+    // const vid = await ytdl.chooseFormat(info.formats,{quality:"highestaudio"})
+    const vid = info.formats.filter(x => x.quality == "tiny")[4]
+    console.log(info.formats)
 
   res.status(200).json({url:vid.url})
 // })
